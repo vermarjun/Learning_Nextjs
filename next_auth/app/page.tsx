dc0b7e513295748f91e22b51e9a170d83b2ee937
vermarjun <vermarjun26@gmail.com>
2025-01-26 15:18:48 +0530

    Next-Auth

---
+++ next_auth/app/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import {SessionProvider, signOut, useSession, signIn} from "next-auth/react"
+
+export default function Home(){
+    return <SessionProvider>
+        <RealHome/>
+    </SessionProvider>
+}
+
+export function RealHome(){
+    const session = useSession()
+
+    return (
+        <div>
+            {session.status == "authenticated" && <button onClick={() => signOut()}>Logout</button>}
+
+            {session.status == "unauthenticated" && <button onClick={() => signIn()}>Sign in</button>}
+
+            {JSON.stringify(session)}
+        </div>
+    )
+}
